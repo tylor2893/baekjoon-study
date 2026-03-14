af5fff400c021ac3e7b209f074b67f6e5dbff19f
tylor2893 <136906432+tylor2893@users.noreply.github.com>
2026-03-14 17:53:02 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.c
@@ -0,0 +1,11 @@
+int main(void) {
+	printf("|\\_/|");
+	printf("\n");
+	printf("|q p|   /}");
+	printf("\n");
+	printf("( 0 )\"\"\"\\");
+	printf("\n");
+	printf("|\"^\"`    |");
+	printf("\n");
+	printf("||_/=\\\\__|");
+}
