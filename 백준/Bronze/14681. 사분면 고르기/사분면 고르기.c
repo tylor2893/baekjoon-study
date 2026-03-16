dc54b8e1bab455fe1913f7d2f8c27584a7add936
tylor2893 <136906432+tylor2893@users.noreply.github.com>
2026-03-16 22:58:49 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void) {
+	int x, y;
+	scanf("%d %d", &x, &y);
+	if (x > 0 && y > 0) {
+		printf("%d", 1);
+	}
+	else if (x < 0 && y > 0) {
+		printf("%d", 2);
+	}
+	else if (x < 0 && y < 0) {
+		printf("%d", 3);
+	}
+	else if (x > 0 && y < 0) {
+		printf("%d", 4);
+	}
+}
