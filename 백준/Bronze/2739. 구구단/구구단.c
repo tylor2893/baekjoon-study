2e4ebe08aa03c9367509bf571962db0e092bbb2f
tylor2893 <136906432+tylor2893@users.noreply.github.com>
2026-03-17 22:47:07 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	int N;
+	scanf("%d", &N);
+	for (int i = 1; i <= 9; ++i) {
+		printf("%d * %d = %d\n",N, i, (N * i));
+	}
+}
