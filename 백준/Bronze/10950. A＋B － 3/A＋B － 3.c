d28e65b2545b6bc461a52b9fd34eb2cf2d4f8f49
tylor2893 <136906432+tylor2893@users.noreply.github.com>
2026-03-18 19:27:54 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	int N;
+	int A;
+	int B;
+
+	
+	scanf("%d", &N);
+
+	for (int i = 0; i < N; ++i) {
+		scanf("%d %d", &A, &B);
+		printf("%d\n", A + B);
+	}
+	
+	
+	return 0;
+}
