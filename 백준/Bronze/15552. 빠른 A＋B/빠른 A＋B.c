cbfd3b04225ba89ee6380da235f822d322073082
tylor2893 <136906432+tylor2893@users.noreply.github.com>
2026-03-18 19:55:44 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 284 ms, Memory: 4896 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.c
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.c
@@ -1,17 +1,18 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
 int main(void) {
 	int N;
 	int A;
 	int B;
 
+	int arr[1000000] = { 0 };
 	scanf("%d", &N);
 
 	for (int i = 0; i < N; ++i) {
 		scanf("%d %d", &A, &B);
-		printf("%d\n", A+B);
+		arr[i] = A + B;
 	}
-	
+	for (int i = 0; i < N; ++i) {
+		printf("%d\n", arr[i]);
+	}
 
 	return 0;
 }
