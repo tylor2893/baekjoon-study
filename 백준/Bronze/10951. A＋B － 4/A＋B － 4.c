811aeaf2b0625c2a0998876bc13f322777d99759
tylor2893 <136906432+tylor2893@users.noreply.github.com>
2026-03-21 10:15:22 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.c
@@ -0,0 +1,14 @@
+#if 1
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	int A, B;
+	while (scanf("%d %d", &A, &B)!=EOF) {
+		printf("%d\n", A + B);
+		
+	}
+	return 0;
+}
+
+#endif
